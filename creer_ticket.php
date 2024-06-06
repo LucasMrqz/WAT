3c1dc703747ed4f0410866ac9a18cc23cf8ebf86
Lucas Marquez <marquezlucas940@gmail.com>
2024-06-06 16:30:21 +0200

    update creer_ticket

--- creer_ticket.php
+++ creer_ticket.php
@@ -7,17 +7,17 @@ if(isset($_POST['boutton-valider'])){
         $titre = htmlspecialchars($_POST['titre']);
         $desc = htmlspecialchars($_POST['desc']);
         $cate = $_POST['categorie'];
-        //$id = $_SESSION['id'];
-        //$Email = $_SESSION['mail'];
-        //$nom = $_SESSION['nom'];
-        
-        //selectionner le bon créateur du ticket
-        //$selectUti = $lien->prepare("SELECT idUtilisateur FROM utilisateur WHERE email = ?");
-        //$selectUti->execute(array($Email));
+        $id = $_SESSION['id'];
+        $Email = $_SESSION['mail'];
+        $nom = $_SESSION['nom'];
+
+        selectionner le bon créateur du ticket
+        $selectUti = $lien->prepare("SELECT idUtilisateur FROM utilisateur WHERE email = ?");
+        $selectUti->execute(array($Email));
 
         // Requête d'insertion des données dans la table ticket
-        $creerTicket = $lien->prepare('INSERT INTO ticket (idCat, titre, description, dateCreation) VALUES (?, ?, ?, NOW())');
-        $creerTicket->execute(array($cate, $titre, $desc));
+        $creerTicket = $lien->prepare('INSERT INTO ticket (idCat, titre, description, dateCreation, idStatut, idUtilisateur) VALUES (?, ?, ?, NOW(), '1', ?)');
+        $creerTicket->execute(array($cate, $titre, $desc, $selectUti['idUtilisateur']));
 
         //header('Location: http://localhost:8080/#/creerTicket');
         header('Location: https://wat.alwaysdata.net/#/creerTicket');
