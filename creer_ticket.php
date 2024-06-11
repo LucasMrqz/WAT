0ea11dc8d110d8d18d3c872a6a35e7d1157ae605
Lucas Marquez <marquezlucas940@gmail.com>
2024-06-11 17:45:07 +0200

    update creer_ticket

--- creer_ticket.php
+++ creer_ticket.php
@@ -3,7 +3,8 @@ require_once('co_bdd.php');
 session_start(); // Assurez-vous de démarrer la session
 
 if(isset($_POST['boutton-valider'])){ 
-    if(isset($_POST['titre']) && isset($_POST['desc']) && isset($_POST['categorie'])) { // les issests servent à verifier que l'utilisateur à bien rempli un login, un mot de passe et qu'il a bien valider ceci
+    if(isset($_POST['idTicket']) && isset($_POST['titre']) && isset($_POST['desc']) && isset($_POST['categorie'])) { 
+        $id = htmlspecialchars($_POST['idTicket']);
         $titre = htmlspecialchars($_POST['titre']);
         $desc = htmlspecialchars($_POST['desc']);
         $cate = $_POST['categorie'];
@@ -16,8 +17,8 @@ if(isset($_POST['boutton-valider'])){
         //$selectUti->execute(array($Email));
 
         // Requête d'insertion des données dans la table ticket
-        $creerTicket = $lien->prepare('INSERT INTO ticket (idCat, titre, description, dateCreation, priorite, idStatut, idUtilisateur) VALUES (?, ?, ?, NOW(), "0", "1", "29")');
-        $creerTicket->execute(array($cate, $titre, $desc));
+        $creerTicket = $lien->prepare('INSERT INTO ticket (idCat, numTicket, titre, description, dateCreation, priorite, idStatut, idUtilisateur) VALUES (?, ?, ?, ?, NOW(), "0", "1", "29")');
+        $creerTicket->execute(array($id, $cate, $titre, $desc));
 
         header('Location: http://localhost:8080/#/creerTicket');
         //header('Location: https://wat.alwaysdata.net/#/creerTicket');
